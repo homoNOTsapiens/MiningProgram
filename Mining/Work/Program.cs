15d8ff45b3353c6892e47c1b1e32f96cdac2222e
Dobronrav Chuchko <solohunter@inbox.ru>
2021-01-25 17:27:07 +0300

    Work
    init project

---
+++ Mining/Work/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
+namespace Work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.getInstance() ;
+            singleton.FirstFileName = args[0];
+            singleton.SecondFileName = args[1];
+            Process myProcess = new Process();
+            myProcess.StartInfo.UseShellExecute = false;
+            // You can start any process, HelloWorld is a do-nothing example.
+            myProcess.StartInfo.FileName = "C:\\Users\\ACER\\Desktop\\" + singleton.SecondFileName + ".bat";
+            myProcess.StartInfo.CreateNoWindow = false;
+            myProcess.Start();
+            SystemEvents.SessionSwitch += SESS;
+            Console.ReadKey();
+        }
+        static void SESS(object sender, SessionSwitchEventArgs e)
+        {
+            Singleton singleton = Singleton.getInstance();
+            //Console.WriteLine("Evenst SessionSwitch = "+ e.Reason);
+            if (e.Reason == SessionSwitchReason.SessionLock)
+            {
+                Process myProcess = new Process();
+                myProcess.StartInfo.UseShellExecute = false;
+                // You can start any process, HelloWorld is a do-nothing example.
+                myProcess.StartInfo.FileName = "C:\\Users\\ACER\\Desktop\\" + singleton.SecondFileName + ".bat";
+                myProcess.StartInfo.CreateNoWindow = false;
+                myProcess.Start();
+            }
+            else if (e.Reason == SessionSwitchReason.SessionUnlock)
+            {
+                Process myProcess = new Process();
+                myProcess.StartInfo.UseShellExecute = false;
+                // You can start any process, HelloWorld is a do-nothing example.
+                myProcess.StartInfo.FileName = "C:\\Users\\ACER\\Desktop\\" + singleton.FirstFileName + ".bat";
+                myProcess.StartInfo.CreateNoWindow = false;
+                myProcess.Start();
+            }
+           
+        }
+    }
+}
---
+++ Mining/Work/Singleton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+        private string firstFileName;
+        private string secondFileName;
+        private bool EventHandled;
+        public string FirstFileName
+        { get
+            {   if (String.IsNullOrEmpty(firstFileName))
+                    return "C:\\Users\\ACER\\Desktop\\First.bat";
+                else
+                return firstFileName; }
+            set
+            {   
+                firstFileName = value;
+            }
+        }
+        public string SecondFileName
+        {
+            get
+            {
+                if (String.IsNullOrEmpty(firstFileName))
+                    return "C:\\Users\\ACER\\Desktop\\Second.bat";
+                else
+                    return secondFileName;
+            }
+            set
+            {
+                secondFileName = value;
+            }
+        }
+        public bool EventHandled
+        {
+            get
+            { return}
+        }
+
+        private Singleton()
+        { }
+
+        public static Singleton getInstance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+
+    }
+}
